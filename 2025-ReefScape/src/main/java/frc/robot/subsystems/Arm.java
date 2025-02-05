98467518721fa4bb786fb636bcfef0df5f2c3b69
Eli-Luk <135551379+Eli-Luk@users.noreply.github.com>
2025-02-04 19:00:28 -0500

    Created new subsytems
    Created the arm and End Effector subsystem.

---
+++ 2025-ReefScape/src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.AnalogInput;
+
+public class Arm {
+    
+//public AnalogInput m_analogEncoder;
+
+    //m_analogEncoder = new AnalogInput(CANID.encoder);
+
+}
