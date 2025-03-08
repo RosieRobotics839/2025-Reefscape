66877ea69b6121a8a5c611729c98e6ecfd606534
ylukin <ylukin@users.noreply.github.com>
2025-03-07 19:52:19 -0500

    waterbury field

--- 2025-ReefScape/src/main/java/frc/robot/Constants.java
+++ 2025-ReefScape/src/main/java/frc/robot/Constants.java
@@ -281,7 +281,7 @@ public final class Constants {
     public static boolean isChampionshipGame = NTBoolean.create(false, "isChampionshipGame", val -> {isChampionshipGame = val; Vision.getInstance().reloadFieldLayout();});
     // Method to get the current field layout path
     public static String getFieldLayoutPath() {
-      return Filesystem.getDeployDirectory() + "/" + "2025-reefscape-andymark.json";
+      return Filesystem.getDeployDirectory() + "/" + "2025-waterbury-practice-field.json";
     }
 
     public static String kFieldLayout = getFieldLayoutPath();
