0469e9ceb22a9ffdf89fb2522b603fb5bca6920f
Eli-Luk <135551379+Eli-Luk@users.noreply.github.com>
2025-04-01 19:00:43 -0400

    Let's Go Rosie!!

---
+++ 2025-ReefScape/src/main/java/frc/utils/Encouragement.java
@@ -0,0 +1,7 @@
+package frc.utils;
+
+public class Encouragement {
+    public void driveTeamEncouragement(String message){
+        System.out.println("You can do this Rosie, LETS GOOO!!");
+    }
+}
