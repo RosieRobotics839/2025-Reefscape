d24b0b0937537d3df3a23526f8bc7ba86b271233
Eli-Luk <135551379+Eli-Luk@users.noreply.github.com>
2025-04-02 18:03:57 -0400

    Easter Egg DLC pack

--- 2025-ReefScape/src/main/java/frc/utils/Encouragement.java
+++ 2025-ReefScape/src/main/java/frc/utils/Encouragement.java
@@ -1,7 +1,18 @@
 package frc.utils;
 
-public class Encouragement {
-    public void driveTeamEncouragement(String message){
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Encouragement extends SubsystemBase{
+
+    public void driveTeamEncouragement(){
+        if (DriverStation.isEnabled()) {
         System.out.println("You can do this Rosie, LETS GOOO!!");
+        }
+    }
+
+    @Override
+    public void periodic() {
+        driveTeamEncouragement();
     }
 }
