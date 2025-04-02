e2dc55de3eaf7026bb4014b185c6eee274a6ace5
Eli-Luk <135551379+Eli-Luk@users.noreply.github.com>
2025-04-02 18:05:45 -0400

    DLC not spamming log

--- 2025-ReefScape/src/main/java/frc/utils/Encouragement.java
+++ 2025-ReefScape/src/main/java/frc/utils/Encouragement.java
@@ -5,9 +5,12 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Encouragement extends SubsystemBase{
 
+public boolean driveTeamIsEncouraged = false;
+
     public void driveTeamEncouragement(){
-        if (DriverStation.isEnabled()) {
+        if (DriverStation.isEnabled() && driveTeamIsEncouraged == false) {
         System.out.println("You can do this Rosie, LETS GOOO!!");
+        driveTeamIsEncouraged = true;
         }
     }
 
