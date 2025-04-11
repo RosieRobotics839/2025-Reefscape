8b819a37ceea65b7aac3320aef821438d646c1cf
Eli-Luk <135551379+Eli-Luk@users.noreply.github.com>
2025-04-10 20:10:36 -0400

    Melodies of Under the sea

--- 2025-ReefScape/src/main/java/frc/robot/Robot.java
+++ 2025-ReefScape/src/main/java/frc/robot/Robot.java
@@ -104,7 +104,7 @@ public class Robot extends TimedRobot {
     songChooser.setDefaultOption("Song 10", Filesystem.getDeployDirectory() + "/" + "song10.chrp");
     songChooser.addOption("Congrats", Filesystem.getDeployDirectory() + "/" + "congrats.chrp");
     songChooser.addOption("Under The Sea", Filesystem.getDeployDirectory() + "/" + "underthesea.chrp");
-    songChooser.addOption("Under The Sea Complex", Filesystem.getDeployDirectory() + "/" + "UnderTheSeaComplex.chrp");
+    songChooser.addOption("Under The Sea Multiple Melodies", Filesystem.getDeployDirectory() + "/" + "undertheseamelodies.chrp");
     songChooser.addOption("SpongeBob Opening Song", Filesystem.getDeployDirectory() + "/" + "spongebobopening.chrp");
     songChooser.addOption("The Jetsons", Filesystem.getDeployDirectory() + "/" + "jetsons.chrp");
     //songChooser.addOption("Wellerman Sea Shanty", Filesystem.getDeployDirectory() + "/" + "wellerman.chrp"); This one sounds good but the other one might sound better, keep both
